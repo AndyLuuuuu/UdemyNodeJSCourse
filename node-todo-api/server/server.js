743a18982957845445445645593ee010ff05e27d
AndyLuuuuu <yilunglu.andy@gmail.com>
2018-03-01 22:12:30 -0800

    20180301

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -100,12 +100,12 @@ app.patch('/todos/:id', (req, res) => {
 // POST /users
 app.post("/users", (req,res) => {
   var body = _.pick(req.body, ["email", "password"]);
-  var newUser = new User(body);
+  var user = new User(body);
 
-  newUser.save().then(() => {
-    return newUser.generateAuthToken();
+  user.save().then(() => {
+    return user.generateAuthToken();
   }).then((token) => {
-    res.header("x-auth", token).send(newUser);
+    res.header("x-auth", token).send(user);
   }).catch((e) => {
     res.status(404).send(e);
   })
@@ -113,6 +113,19 @@ app.post("/users", (req,res) => {
 
 app.get("/users/me", authenticate, (req,res) => {
   res.send(req.user);
+});
+
+// POST /users/login {email, password}
+app.post("/users/login", (req,res) => {
+  var body = _.pick(req.body, ["email", "password"]);
+  
+  User.findbyCredentials(body.email, body.password).then ((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header("x-auth", token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
